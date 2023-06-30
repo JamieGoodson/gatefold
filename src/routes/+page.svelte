59af2a08d4dc57894d9e686d8e1d44a7fb119a61
Jamie Goodson <goodsonjamie@gmail.com>
2023-06-30 23:48:33 +0100

    Remove album release year

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	interface Track {
 		albumImages: string[];
-		albumReleaseYear: string;
 		artists: string[];
 		name: string;
 	}
 
 	const track: Track = {
 		albumImages: ['/steely-dan-cover.jpg'],
-		albumReleaseYear: '1972',
 		artists: ['Steely Dan', 'Donald Fagen', 'Walter Becker'],
 		name: 'Dirty Work'
 	};
@@ -19,6 +17,5 @@
 	<div class="flex flex-col gap-y-3 w-1/2">
 		<div class="text-6xl font-extrabold">{track.name}</div>
 		<div class="text-neutral-400 text-2xl font-medium">{track.artists.join(', ')}</div>
-		<div class="text-neutral-500 text-xl font-medium">{track.albumReleaseYear}</div>
 	</div>
 </div>
