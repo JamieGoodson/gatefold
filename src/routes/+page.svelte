a2307c882fa45cfcb0e9cb994f8ba9c9ff01a458
Jamie Goodson <goodsonjamie@gmail.com>
2023-06-30 23:53:13 +0100

    Improve layout sizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,8 +12,10 @@
 	};
 </script>
 
-<div class="w-full h-full flex gap-x-6 items-center justify-center box-border">
-	<img src={track.albumImages[0]} class="max-w-lg w-1/2 rounded-lg mb-2" />
+<div class="w-full h-full flex gap-x-6 items-center justify-center">
+	<div class="flex w-1/2 justify-end">
+		<img class="w-full max-w-lg rounded-lg mb-2" src={track.albumImages[0]} />
+	</div>
 	<div class="flex flex-col gap-y-3 w-1/2">
 		<div class="text-6xl font-extrabold">{track.name}</div>
 		<div class="text-neutral-400 text-2xl font-medium">{track.artists.join(', ')}</div>
